063ac664b727277e8f0416aaeabfe82b5b045064
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2024-01-23 13:07:06 +0545

    refactor the code

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ import {
 
 console.log("new app working...");
 
+/**
+ * add submit event
+ * add keyup event
+ * update ui
+ * show/hide button
+ * delete button
+ * edit/update button
+ */
 addEventListenerToShowHideButton();
 addSubmitEventListenerToForm();
 addKeyUpEventListenerToForm();
